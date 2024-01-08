7da9177067febcc58da0b9acb6e7cb0a4ba4d9b2
vinith <vinithp100@gmail.com>
2024-01-08 13:50:41 +0530

    push to github

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse} from 'next/server'
 export async function GET(req: NextRequest){
     try {
         const response = NextResponse.json({message: `successfully logged out`, value:Math.random()},{status: 200})
-        response.headers.set('Cache-Control', 'no-cache')
+        response.headers.set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
         response.cookies.set('token', '',{httpOnly: true})
         return response
 
